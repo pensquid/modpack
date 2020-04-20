8bc6a38b5e093f52fc33ab5356ead27260bbb9f8
Ulisse Mini <ulisse.mini@gmail.com>
2020-04-19 23:57:01 -0400

    Add autocrafting prototype

---
+++ code/craft_transistor.lua
@@ -0,0 +1,45 @@
+local component = require('component')
+local crafting = component.crafting
+local inv = component.inventory_controller
+local sides = require('sides')
+local robot = require('robot')
+
+local iron = 'minecraft:iron_ingot'
+local goldn = 'minecraft:gold_nugget'
+local paper = 'minecraft:paper'
+local redstone = 'minecraft:redstone'
+local recipe = {
+  iron, iron, iron,
+  goldn, paper, goldn,
+  false, redstone, false,
+}
+
+local function inTable(val, tabl)
+  for i,v in ipairs(tabl) do
+    if v == val then return true end
+  end
+  return false
+end
+
+for i,v in ipairs(recipe) do print(i,v) end
+
+for slot = 1,54 do
+  local info = inv.getStackInSlot(sides.bottom, slot)
+  if info ~= nil then
+  print(info['name'])
+  for i,v in ipairs(recipe) do
+    if v and v == info['name'] then
+      print('found ' .. v)
+      recipe[i] = false
+      i = i + i // 4
+      if i > 11 then error('too moch') end
+      print('select:', i)
+      robot.select(i)
+      assert(
+        inv.suckFromSlot(sides.bottom, slot, 1))
+    end
+  end
+  end
+end
+
+crafting.craft()
